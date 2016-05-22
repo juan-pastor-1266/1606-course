040d7100fd23b1b13ac11609bae1bca6edb6aa8f
juan-pastor-1266 <japf1266@gmail.com>
2016-05-22 17:03:44 +0200

    feat:list_view
    List View added --- NO Event Handler

--- app/src/main/java/com/example/japf/myhelloworld/MainActivityFragment.java
+++ app/src/main/java/com/example/japf/myhelloworld/MainActivityFragment.java
@@ -5,19 +5,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    List<String> contactsList;
+    ArrayAdapter<String> contactsListAdapter;
+
     public MainActivityFragment() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Fake data to populate the list of contacts
+        String[] names ={
+                "John",
+                "Mary",
+                "Joaquin",
+                "Jane",
+                "Elena",
+                "Maurice",
+                "Filemon",
+                "Teofila"
+        };
+
+        contactsList = new ArrayList<String>(Arrays.asList(names));
+        contactsListAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),        // The current context
+                        R.layout.my_list_row, // layout id
+                        R.id.contact_name,    // textview to populate.
+                        names);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+        listView.setAdapter(contactsListAdapter);
+
+        return rootView;
     }
 }
